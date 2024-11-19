4ec868b7e5e2e4a672a51b5adc24fd174b92a76d
Muhammad Rafi Munggaran <ethanjeger123@gmail.com>
2024-11-19 14:01:16 +0700

    final

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./Routes/auth.routes.js";
 import movieRoutes from "./Routes/movie.routes.js";
@@ -13,6 +14,8 @@ import { protectRoute } from "./middlewares/protectRoute.js";
 const app = express();
 const PORT = ENV_VARS.PORT;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -24,6 +27,14 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvSeriesRoutes);
 app.use("/api/v1/search", protectRoute, searchRoutes);
+
+if (ENV_VARS.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log("Server started at http://localhost:" + PORT);
--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,21 +1,21 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useAuthStore } from "../store/authUser";
 
 const SignupPage = () => {
-  // const { searchParams } = new URL(document.location);
-  // const emailValue = searchParams.get("email");
+  const { searchParams } = new URL(document.location);
+  const emailValue = searchParams.get("email");
 
-  // const [email, setEmail] = useState(emailValue || "");
-  const [email, setEmail] = useState("");
+  const [email, setEmail] = useState(emailValue || "");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  // const { signup, isSigningUp } = useAuthStore();
+  const { signup, isSigningUp } = useAuthStore();
 
   const handleSignUp = (e) => {
     e.preventDefault();
-    console.log(email, username, password);
-    // signup({ email, username, password });
+    // console.log(email, username, password);
+    signup({ email, username, password });
   };
   return (
     <div className="h-screen w-full hero-bg">
@@ -87,10 +87,9 @@ const SignupPage = () => {
               className="w-full py-2 bg-red-600 text-white font-semibold rounded-md
 							hover:bg-red-700
 						"
-              // disabled={isSigningUp}
+              disabled={isSigningUp}
             >
-              Signup
-              {/* {isSigningUp ? "Loading..." : "Sign Up"} */}
+              {isSigningUp ? "Loading..." : "Sign Up"}
             </button>
           </form>
           <div className="text-center text-gray-400">
